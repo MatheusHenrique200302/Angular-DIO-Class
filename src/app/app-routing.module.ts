2bfe0ad9cb8d620af5348fc1ea769074080d496c
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-13 15:26:32 -0300

    added error404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { Error404Component } from './error404/error404.component';
 
 const routes: Routes = [
 
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path:'courses',
     component: CoursesListComponent
+  },
+  {
+    path: "**",
+    component: Error404Component
   }
 
 
