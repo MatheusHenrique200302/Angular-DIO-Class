b7fa7705a5a4f4001df6e3713e763f7e9636bff3
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-13 12:10:37 -0300

    Added Forms module and course filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesListComponent } from './courses/courses-list/courses-list.component';
@@ -14,7 +14,9 @@ import { StarComponent } from './star/star.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -8,11 +8,30 @@ import { CoursesService } from '../courses.service';
   styleUrls: ['./courses-list.component.css'],
 })
 export class CoursesListComponent implements OnInit {
-  courses: Course[] = [];
+
+  
+  
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
+
+  _filterby: string = "";
 
   constructor(private courselist: CoursesService) {}
 
+  
+
   ngOnInit(): void {
-    this.courses = this.courselist.retrieveall();
+    this._courses = this.courselist.retrieveall();
+    this.filteredCourses = this._courses;
   }
+
+  set filter(value: string){
+    this._filterby=value;
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLowerCase().indexOf(this._filterby.toLocaleLowerCase()) > -1);
+  }
+
+  get filter(){
+    return this._filterby;
+  }
+
 }
